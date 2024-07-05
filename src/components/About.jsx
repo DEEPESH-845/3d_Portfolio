e782618b45590d7c4156eb2f74401cf32b2679b7
DEEPESH-845 <dkakkar_be23@thapar.edu>
2024-07-05 19:16:25 +0530

    x1 commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,16 +14,16 @@ const ServiceCard = ({ index, title, icon }) => {
     <Tilt className="xs:w-[250px] w-full">
       <motion.div 
           variants={fadeIn("right", "spring", 0.5*index ,0.75)}     
-          className='w-full green-pink-gradient p-[1px] rounded-[20px]shadow-card'  
+          className='w-full green-pink-gradient p-[1px] rounded-[20px]'  
         >
-          <div 
+          <div  
             options={{
                max: 45 ,
                scale: 1,
                speed: 450 
 
             }}
-            className='bg-tertiary rounded-[20px]  py-5 px-12 min-h-[280px] flex justify-evenly items-center  flex-col' 
+            className='bg-tertiary rounded-[20px]  py-5 px-12 min-h-[280px] flex justify-evenly items-center  flex-col  '
           >
             <img src={icon} alt={title}
             className='w-16 h-16 object-contain' />
